0a86043826315f609ba94cc0411faefb6074a863
Sanjeev Thiyagarajan <sanjeevkt720@gmail.com>
2021-08-14 23:14:28 -0400

    Lesson 15

---
+++ main.py
@@ -0,0 +1,31 @@
+from typing import Optional
+from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your posts"}
+
+
+@app.post("/createposts")
+def create_posts(post: Post):
+    print(post)
+    print(post.dict())
+    return {"data": post}
+# title str, content str
