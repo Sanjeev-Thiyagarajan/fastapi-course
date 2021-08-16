73f1a023ea34a150a284e4e6f50faf79ea869113
Sanjeev Thiyagarajan <sanjeevkt720@gmail.com>
2021-08-15 23:12:12 -0400

    Lesson 10

--- app/main.py
+++ app/main.py
@@ -71,3 +71,17 @@ def delete_post(id: int):
 
     my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} does not exist")
+
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+    return {"data": post_dict}
