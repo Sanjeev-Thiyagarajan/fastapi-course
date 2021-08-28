1b75aaecf7c8abf45678d19753aac2b8ee787b98
Sanjeev Thiyagarajan <sanjeevkt720@gmail.com>
2021-08-27 21:35:44 -0400

    Lesson 15

--- app/main.py
+++ app/main.py
@@ -144,3 +144,13 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)
 
     return new_user
+
+
+@app.get('/users/{id}', response_model=schemas.UserOut)
+def get_user(id: int, db: Session = Depends(get_db), ):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"User with id: {id} does not exist")
+
+    return user
