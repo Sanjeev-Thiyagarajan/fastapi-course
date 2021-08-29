df725e556093bcc4fe24f85716ec29dff6a293c3
Sanjeev Thiyagarajan <sanjeevkt720@gmail.com>
2021-08-28 22:47:15 -0400

    Lesson 21

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
-from typing import List
+from typing import List, Optional
 from .. import models, schemas, oauth2
 from ..database import get_db
 
@@ -12,11 +12,12 @@ router = APIRouter(
 
 
 @router.get("/", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int = 10, skip: int = 0, search: Optional[str] = ""):
     # cursor.execute("""SELECT * FROM posts """)
     # posts = cursor.fetchall()
-
-    posts = db.query(models.Post).all()
+    print(search)
+    posts = db.query(models.Post).filter(
+        models.Post.title.contains(search)).limit(limit).offset(skip).all()
     return posts
 
 
