e1818d85954366f4dd1fbaea53477af5499e611c
Sanjeev Thiyagarajan <sanjeevkt720@gmail.com>
2021-08-31 02:13:47 -0400

    added requirements.txt

--- app/main.py
+++ app/main.py
@@ -1,14 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from . import models
 from .database import engine
 from .routers import post, user, auth, vote
 from .config import settings
 
-print(settings.database_username)
 
 # models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ["https://www.google.com", "https://www.youtube.com"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
