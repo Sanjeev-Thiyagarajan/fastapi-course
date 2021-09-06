7966f06a7d275e4fae68e79ecf1e02212b47fbdd
Sanjeev Thiyagarajan <sanjeevkt720@gmail.com>
2021-09-06 01:18:11 -0400

    before testing

--- app/database.py
+++ app/database.py
@@ -8,6 +8,7 @@ from .config import settings
 
 SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
