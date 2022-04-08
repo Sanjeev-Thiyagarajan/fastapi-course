daec62a4df1de76175a9597f9e877cfee9ac371a
dhawalpanchal981 <94890424+dhawalpanchal981@users.noreply.github.com>
2022-04-08 17:00:16 +0530

    work

--- app/oauth2.py
+++ app/oauth2.py
@@ -8,13 +8,9 @@ from .config import settings
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
-# SECRET_KEY
-# Algorithm
-# Expriation time
-
-SECRET_KEY = settings.secret_key
-ALGORITHM = settings.algorithm
-ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 180
 
 
 def create_access_token(data: dict):
--- app/schemas.py
+++ app/schemas.py
@@ -2,8 +2,6 @@ from pydantic import BaseModel, EmailStr
 from datetime import datetime
 from typing import Optional
 
-from pydantic.types import conint
-
 
 class PostBase(BaseModel):
     title: str
@@ -59,8 +57,3 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     id: Optional[str] = None
-
-
-class Vote(BaseModel):
-    post_id: int
-    dir: conint(le=1)
